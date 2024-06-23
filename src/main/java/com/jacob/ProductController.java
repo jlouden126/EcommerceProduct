1c893623a38fa7ab565345b3b094532ca890e58a
jlouden126 <jlouden126@gmail.com>
2024-06-23 17:47:30 -0400

    Added selenium with docker

--- src/main/java/com/jacob/ProductController.java
+++ src/main/java/com/jacob/ProductController.java
@@ -3,17 +3,26 @@ package com.jacob;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.net.MalformedURLException;
 import java.util.List;
+import java.lang.Thread;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 @Controller
 public class ProductController {
 
-    @GetMapping("/products")
-    public String getProducts(Model model) {
+
+    @GetMapping("/")
+    public String getProducts(Model model) throws InterruptedException, MalformedURLException, URISyntaxException{
+        System.out.println("YAAY");
         List<Product> products = null;
         try {
-            products = AmazonScraper.scrapeAmazonProducts("https://www.amazon.com/s?k=soccer");
+            products = AmazonScraper.scrapeAmazonProducts("https://www.amazon.com/dp");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -22,3 +31,18 @@ public class ProductController {
         return "productList";
     }
 }
+
+//     @GetMapping("/products")
+//     public String getProducts(Model model) {
+//         System.out.println("YAAY");
+//         List<Product> products = null;
+//         try {
+//             products = AmazonScraper.scrapeAmazonProducts("https://www.amazon.com/s?k=soccer");
+//         } catch (InterruptedException e) {
+//             e.printStackTrace();
+//         }
+
+//         model.addAttribute("products", products);
+//         return "productList";
+//     }
+// }
